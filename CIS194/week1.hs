0a38a037c66bad6eea8b44e43722b5d129dec254
Dev Joshi <devjtitan@gmail.com>
2024-08-29 23:07:29 +0530

    remove extra space in front of every line

--- CIS194/week1.hs
+++ CIS194/week1.hs
@@ -1,25 +1,27 @@
- double :: Int -> Int
- double x = x * 2
+double :: Int -> Int
+double x = x * 2
 
- alternate :: Bool -> [Char] -> [Int]
- alternate _ [] = [] 
- alternate _ [c] = [read [c]]
- alternate True (c:_:cs) = (read [c]) : alternate True cs
- alternate False (_:c:cs) = (read [c]) : alternate False cs
- 
- doubleIntArr :: [Int] -> [Int]
- doubleIntArr [] = []
- doubleIntArr (x:xs) = (2*x) : doubleIntArr xs
+alternate :: Bool -> [Char] -> [Int]
+alternate _ [] = [] 
+alternate _ [c] = [read [c]]
+alternate True (c:_:cs) = (read [c]) : alternate True cs
+alternate False (_:c:cs) = (read [c]) : alternate False cs
 
- addDigits :: Int -> Int
- addDigits x
-  | x <= 9    = x
-  | otherwise = (mod x 10) + addDigits (div x 10)
+doubleIntArr :: [Int] -> [Int]
+doubleIntArr [] = []
+doubleIntArr (x:xs) = (2*x) : doubleIntArr xs
 
- main :: IO()
- main = do
-   let cardNumber = "3333000030303333"
-   print $ addDigits (1000000)
-   print $ alternate True cardNumber
-   print $ alternate False cardNumber
+addDigits :: Int -> Int
+addDigits x
+ | x <= 9    = x
+ | otherwise = (mod x 10) + addDigits (div x 10)
+
+
+
+main :: IO()
+main = do
+  let cardNumber = "3333000030303333"
+  print $ addDigits (1000000)
+  print $ alternate True cardNumber
+  print $ alternate False cardNumber
 
