b2479a00a44c78e8d6b825da58df70140653b0e3
Dev Joshi <devjtitan@gmail.com>
2024-08-30 08:16:49 +0530

    complete week 1 solution for CIS194 course
    - use composition to chain multiple function calls

--- CIS194/week1.hs
+++ CIS194/week1.hs
@@ -2,26 +2,28 @@ double :: Int -> Int
 double x = x * 2
 
 alternate :: Bool -> [Char] -> [Int]
-alternate _ [] = [] 
+alternate _ [] = []
 alternate _ [c] = [read [c]]
 alternate True (c:_:cs) = (read [c]) : alternate True cs
 alternate False (_:c:cs) = (read [c]) : alternate False cs
-
-doubleIntArr :: [Int] -> [Int]
-doubleIntArr [] = []
-doubleIntArr (x:xs) = (2*x) : doubleIntArr xs
 
 addDigits :: Int -> Int
 addDigits x
  | x <= 9    = x
  | otherwise = (mod x 10) + addDigits (div x 10)
 
-
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
 
 main :: IO()
 main = do
   let cardNumber = "3333000030303333"
-  print $ addDigits (1000000)
-  print $ alternate True cardNumber
-  print $ alternate False cardNumber
-
+  -- Without `$`, `alternate` could be composed
+  -- only if a single argument is supplied.
+  let x = sumList . map addDigits . map double $ alternate True cardNumber
+      y = sumList $ alternate False cardNumber
+      rem = mod (x+y) 10
+  if rem == 0
+    then putStrLn "Card number is valid."
+    else putStrLn "Card number is invalid"
