da2f03125dc1c56b635c74a1b33b37704f57030e
Dev Joshi <devjtitan@gmail.com>
2024-10-09 22:17:04 +0530

    add pattern-matching example in ADTs

--- baby.hs
+++ baby.hs
@@ -7,9 +7,28 @@ isEven x = if mod x 2 == 0
 -- Apparently GHCI throws errors if you use tabs over spaces.
 -- Also, you need to use `"` instead of `'` for strings.
 
+data SomeType = Shoe
+ | Ship
+ | SealingWax
+ deriving Show
+
+data Foo = Foo String Int
+ deriving Show
+
+-- `@` is used to extract values inside the `Foo` data type.
+func :: Foo -> String
+func f@(Foo str _) = "Pattern matching is " ++ str
+
+shoe :: SomeType
+shoe = Shoe
+
+john :: Foo
+john = Foo "cool" 23
+
 main :: IO()
 main = do
-  putStrLn $ show (y)
+  putStrLn $ show (shoe)
+  putStrLn $ func john
   -- `$` usage: `f $ x` is equivalent to `f(x)`
   -- So, `putStrLn $ show (isEven 4)` is the same
   -- as `putStrLn (show (isEven 4))`
