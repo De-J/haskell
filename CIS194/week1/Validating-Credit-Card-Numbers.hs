cb1e2dba6501b00f873a6cc831e5b89637674943
Dev Joshi <devjtitan@gmail.com>
2024-12-07 23:13:21 +0530

    refactors

--- CIS194/week1/Validating-Credit-Card-Numbers.hs
+++ CIS194/week1/Validating-Credit-Card-Numbers.hs
@@ -1,30 +1,41 @@
+-- Exercise 1
+toDigits :: Integer -> [Integer]
+toDigits x
+  | x <= 0 = []
+  | otherwise = toDigits (div x 10) ++ [mod x 10]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x
+  | x <= 0 = []
+  | otherwise = mod x 10 : toDigitsRev (div x 10)
+
 double :: Int -> Int
 double x = x * 2
+
+-- Exercise 2
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (a : b : cs) = [a * 2, b] ++ doubleEveryOther cs
 
 alternate :: Bool -> [Char] -> [Int]
 alternate _ [] = []
 alternate _ [c] = [read [c]]
-alternate True (c:_:cs) = (read [c]) : alternate True cs
-alternate False (_:c:cs) = (read [c]) : alternate False cs
-
-addDigits :: Int -> Int
-addDigits x
- | x <= 9    = x
- | otherwise = (mod x 10) + addDigits (div x 10)
+alternate True (c : _ : cs) = read [c] : alternate True cs
+alternate False (_ : c : cs) = read [c] : alternate False cs
 
 sumList :: [Int] -> Int
 sumList [] = 0
-sumList (x:xs) = x + sumList xs
+sumList (x : xs) = x + sumList xs
 
-main :: IO()
+main :: IO ()
 main = do
   let cardNumber = "3333000030303333"
   -- Without `$`, `alternate` could be composed
   -- only if a single argument is supplied.
-  let x = sumList . map addDigits . map double $ alternate True cardNumber
-      y = sumList $ alternate False cardNumber
-      rem = mod (x+y) 10
-  if rem == 0
-    then putStrLn "Card number is valid."
-    else putStrLn "Card number is invalid"
-
+  -- let x = sumList . map toDigits . map double $ alternate True cardNumber
+  -- y = sumList $ alternate False cardNumber
+  -- rem = mod (x+y) 10
+  -- if rem == 0
+  -- then putStrLn "Card number is valid."
+  -- else putStrLn "Card number is invalid"
+  putStrLn "Card number is valid."
--- baby.hs
+++ baby.hs
@@ -1,19 +1,24 @@
 doubleMe x = x + x
 
-isEven x = if mod x 2 == 0
-                  then True
-                  else False
+isEven x =
+  if mod x 2 == 0
+    then True
+    else False
 
 -- Apparently GHCI throws errors if you use tabs over spaces.
 -- Also, you need to use `"` instead of `'` for strings.
 
-data SomeType = Shoe
- | Ship
- | SealingWax
- deriving Show
+data SomeType
+  = Shoe
+  | Ship
+  | SealingWax
+  deriving (Show)
+
+-- Without using `deriving Show` you cannot use the `show`
+-- function on `SomeType` data type.
 
 data Foo = Foo String Int
- deriving Show
+  deriving (Show)
 
 -- `@` is used to extract values inside the `Foo` data type.
 func :: Foo -> String
@@ -25,10 +30,11 @@ shoe = Shoe
 john :: Foo
 john = Foo "cool" 23
 
-main :: IO()
+main :: IO ()
 main = do
-  putStrLn $ show (shoe)
+  print shoe
   putStrLn $ func john
-  -- `$` usage: `f $ x` is equivalent to `f(x)`
-  -- So, `putStrLn $ show (isEven 4)` is the same
-  -- as `putStrLn (show (isEven 4))`
+
+-- `$` usage: `f $ x` is equivalent to `f(x)`
+-- So, `putStrLn $ show (isEven 4)` is the same
+-- as `putStrLn (show (isEven 4))`
